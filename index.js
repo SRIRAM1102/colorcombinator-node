31be3df99de281da30b2344bf67b4ddd8b2cc75b
SRIRAM1102 <85927573+SRIRAM1102@users.noreply.github.com>
2021-11-03 12:18:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import {MongoClient,ObjectId} from "mongodb";
 import cors from "cors";
 import  jwt  from "jsonwebtoken";
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
 // import {auth} from "./middleware/auth.js"
 
 const app=express();
@@ -61,28 +61,28 @@ app.post("/signup",async(req,res)=>{
 
          
            
-var transporter = nodemailer.createTransport({
-  service: 'outlook', 
-  auth: {
-    user: 'sriramsaravanan11@outlook.com',
-    pass: 'Sriram4924'
-  }
-});           
+// var transporter = nodemailer.createTransport({
+//   service: 'outlook', 
+//   auth: {
+//     user: 'sriramsaravanan11@outlook.com',
+//     pass: 'Sriram4924'
+//   }
+// });           
 
-var mailOptions = {  
-  from: 'sriramsaravanan11@outlook.com',
-  to: emailId,
-  subject: 'Welcome message',
-  text: 'Colorcombinator welcomes you!!'
-};
+// var mailOptions = {  
+//   from: 'sriramsaravanan11@outlook.com',
+//   to: emailId,
+//   subject: 'Welcome message',
+//   text: 'Colorcombinator welcomes you!!'
+// };
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else { 
-    console.log('Email sent: ' + info.response);
-  }
-});
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else { 
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
     }
 else{
   res.send({msg:"existing mailid"})
